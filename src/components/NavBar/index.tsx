d4c671c4ab215a50407ab20fc8d038c60b7f4de1
kentur <kevinxrturop@gmail.com>
2023-10-08 15:01:26 -0500

    Update index.tsx

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -70,19 +70,13 @@ export const PageTabs = () => {
       <MenuItem href={`/tokens/${chainName.toLowerCase()}`} isActive={pathname.startsWith('/tokens')}>
         <Trans>Tokens</Trans>
       </MenuItem>
-      {!shouldDisableNFTRoutes && (
-        <MenuItem dataTestId="nft-nav" href="/nfts" isActive={isNftPage}>
-          <Trans>NFTs</Trans>
-        </MenuItem>
-      )}
+      
       <Box display={{ sm: 'flex', lg: 'none', xxl: 'flex' }} width="full">
         <MenuItem href="/pools" dataTestId="pool-nav-link" isActive={isPoolActive}>
           <Trans>Pools</Trans>
         </MenuItem>
       </Box>
-      <Box marginY="4">
-        <MenuDropdown />
-      </Box>
+     
     </>
   )
 }
