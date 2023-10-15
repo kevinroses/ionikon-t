8d9ee559ad722aa101aa1ca8f0f56de2b52f4955
kentur <kevinxrturop@gmail.com>
2023-10-15 10:18:34 -0500

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
-export const UNI_LIST = './tokenLists/tokenlist.json'
+export const UNI_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
 
 export const UNI_EXTENDED_LIST = 'https://gateway.ipfs.io/ipns/extendedtokens.uniswap.org'
 const UNI_UNSUPPORTED_LIST = 'https://gateway.ipfs.io/ipns/unsupportedtokens.uniswap.org'
